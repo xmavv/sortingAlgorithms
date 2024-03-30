acff9374f6c3536af7cebf9902092615ca59ada5
xmavv <xxmatheooo@gmail.com>
2024-03-30 02:55:30 +0100

    menu corrections, validations, copying array

--- Sources/Menu.cpp
+++ Sources/Menu.cpp
@@ -1,5 +1,5 @@
 #include "../Headers/Menu.h"
-#include "../Headers/Print.h"
+#include "../Headers/Utilities.h"
 #include "stdio.h"
 #include "iostream"
 #include "time.h"
@@ -9,25 +9,36 @@
 using namespace std;
 
 void Menu::start() {
-    cout<<"witaj w projekcie sortowanie!"<<endl;
-    cout<<endl<<"wybierz sposob podania danych"<<endl;
-    cout<<"1. wygeneruj losowo dane o zadanej dlugosci"<<endl;
-    cout<<"2. wczytaj dane z pliku"<<endl;
-    cin>>userChoice;
+    string fileName;
 
-    if(userChoice == 1) {
-        cout<<"podaj rozmiar tablicy"<<endl;
-        cin>>arrayLength;
-        this->generateRandomArray(arrayLength); //global arrayToSort variable changed, so we can work with it
-        this->chooseAlgorithm();
-    } else if(userChoice == 2) {
-        string fileName;
+    while(userChoice != 1 || userChoice!= 2) {
+        cout<<"witaj w projekcie sortowanie!"<<endl;
+        cout<<endl<<"wybierz sposob podania danych (korzystaj tylko z cyfr!)"<<endl;
+        cout<<"1. wygeneruj losowo dane o zadanej dlugosci"<<endl;
+        cout<<"2. wczytaj dane z pliku"<<endl;
+        cout<<"0. wyjdz z programu"<<endl;
+        cin>>userChoice;
 
-        cout<<"podaj nazwe pliku (plik musi znajdowac sie w folderze /cmake-build-debug/!)"<<endl;
-        cin>>fileName;
-        this->loadArrayFromFile(fileName); //global arrayToSort variable changed, so we can work with it
-        this->chooseAlgorithm();
-    } //validation
+        switch(userChoice) {
+            case 1:
+                cout<<"podaj rozmiar tablicy"<<endl;
+                cin>>arrayLength;
+                generateRandomArray(arrayLength); //global arrayToSort variable changed, so we can work with it
+                chooseAlgorithm();
+                break;
+            case 2:
+                cout<<"podaj nazwe pliku (plik musi znajdowac sie w folderze /cmake-build-debug/!)"<<endl;
+                cin>>fileName;
+                loadArrayFromFile(fileName); //global arrayToSort variable changed, so we can work with it
+                chooseAlgorithm();
+                break;
+            case 0:
+                exit(0);
+            default:
+                cerr<<"niepoprawny wybor!"<<endl;
+                break;
+        }
+    }
 }
 
 void Menu::generateRandomArray(int len){
@@ -41,7 +52,7 @@ void Menu::generateRandomArray(int len){
         pointer++;
     }
 
-    Print::printArray(arrayToSort, len, "twoja tablica");
+    Utilities::printArray(arrayToSort, len, "twoja tablica");
 }
 
 void Menu::loadArrayFromFile(string name) {
@@ -67,32 +78,60 @@ void Menu::loadArrayFromFile(string name) {
         pointer++;
     }
 
-    Print::printArray(arrayToSort, arrayLength, "twoja tablica");
+    Utilities::printArray(arrayToSort, arrayLength, "twoja tablica");
 }
 
 void Menu::chooseAlgorithm() {
-    cout<<"wybierz algorytm"<<endl;
-    cout<<"1. insertionsort"<<endl;
-    cout<<"2. quicksort"<<endl;
-    cout<<"3. shellsort"<<endl;
-    cout<<"4. heapsort"<<endl;
-    cin>>userChoice;
+    int originalArray[arrayLength]; //copying original array to delete the pointer
+    Utilities::copyArray(arrayToSort, originalArray, arrayLength);
+    delete [] arrayToSort;
 
-    if(userChoice == 1) {
-        algorithm = &insertionsort;
-        algorithm -> sort(arrayToSort, arrayLength);
-        cout<<"wybrales"<<userChoice<<"algorytm"<<endl;
-    } else if (userChoice == 2) {
-        algorithm = &quicksort;
-        algorithm -> sort(arrayToSort, arrayLength);
-        cout<<"wybrales"<<userChoice<<"algorytm"<<endl;
-    } else if (userChoice == 3) {
-        algorithm = &shellsort;
-        algorithm -> sort(arrayToSort, arrayLength);
-        cout<<"wybrales"<<userChoice<<"algorytm"<<endl;
-    } else if (userChoice == 4) {
-        algorithm = &heapsort;
-        algorithm -> sort(arrayToSort, arrayLength);
-        cout<<"wybrales"<<userChoice<<"algorytm"<<endl;
-    } //validation
+    while(userChoice != 0) {
+        int arrayCopy[arrayLength]; //copying "original" array each time, so I have access to the one generated or loaded
+        Utilities::copyArray(originalArray, arrayCopy, arrayLength);
+
+        cout<<"wybierz algorytm:"<<endl<<endl;
+        cout<<"1. insertionsort"<<endl;
+        cout<<"2. quicksort"<<endl;
+        cout<<"3. shellsort"<<endl;
+        cout<<"4. heapsort"<<endl<<endl;
+        cout<<"lub: "<<endl<<endl;
+        cout<<"5. pokaz aktualna tablice"<<endl;
+        cout<<"0. wyjdz do menu"<<endl;
+        cin>>userChoice;
+
+        switch (userChoice) {
+            case 1:
+                cout<<"wybrales 1 algorytm"<<endl;
+                algorithm = &insertionsort;
+                algorithm -> sort(arrayCopy, arrayLength);
+                break;
+            case 2:
+                cout<<"wybrales 2 algorytm"<<endl;
+                algorithm = &quicksort;
+                algorithm -> sort(arrayCopy, arrayLength);
+                break;
+            case 3:
+                cout<<"wybrales 3 algorytm"<<endl;
+                algorithm = &shellsort;
+                algorithm -> sort(arrayCopy, arrayLength);
+                break;
+            case 4:
+                cout<<"wybrales 4 algorytm"<<endl;
+                algorithm = &heapsort;
+                algorithm -> sort(arrayCopy, arrayLength);
+                break;
+            case 5:
+                Utilities::printArray(arrayCopy, arrayLength, "twoja aktualna tablica");
+                break;
+            case 0:
+                start();
+                break;
+            default:
+                cerr<<"niepoprawny wybor!"<<endl;
+                break;
+        }
+    }
+
+    return;
 }
