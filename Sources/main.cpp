a50a2271537c338840b6aa41944c5d502d57d880
xmavv <xxmatheooo@gmail.com>
2024-04-01 01:08:40 +0200

    implementation of template type in main.cpp

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -91,8 +91,31 @@ float avg(float *array, int ile){
 }
 
 int main() {
+    int variableType;
 
-    Menu menu;
-    menu.chooseArray();
+    while(variableType != 1 || variableType != 2 || variableType != 3) {
+        cout << "witaj w projekcie sortowanie!" << endl << endl;
+        cout << "--------------------MENU-1--------------------" << endl << endl;
+        cout << endl << "wybierz typ danych na ktorym chcesz pracowac" << endl;
+        cout << "1. int" << endl;
+        cout << "2. float" << endl;
+        cout << "3. char" << endl;
+        cout << "0. wyjdz z programu" << endl;
+        cin>>variableType;
+
+        if(variableType == 1) {
+            Menu<int> menu1(variableType);
+            menu1.chooseArray();
+        } else if(variableType == 2) {
+            Menu<float> menu2(variableType);
+            menu2.chooseArray();
+        } else if(variableType == 3) {
+            Menu<char> menu3(variableType);
+            menu3.chooseArray();
+        } else {
+            exit(0);
+        }
+    }
+
     return 0;
 }
