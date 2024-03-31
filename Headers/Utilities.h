42dfc81b1d5dee4cab19cd0b92726e6543d2b3c3
xmavv <xxmatheooo@gmail.com>
2024-03-31 01:09:50 +0100

    add precise clock to count time for each algorithm

--- Headers/Utilities.h
+++ Headers/Utilities.h
@@ -18,10 +18,14 @@
 using namespace std;
 
 class Utilities {
+    double PCFreq = 0.0;
+    __int64 start = 0;
 public:
     Utilities();
     static void printArray(int *arr, int len, string mes);
     static void copyArray(int *arr, int *arrayCopy, int len);
+    void startCounter();
+    double getCounter();
 };
 
 
--- Sources/Utilities.cpp
+++ Sources/Utilities.cpp
@@ -3,6 +3,7 @@
 #include "iostream"
 #include "time.h"
 #include "cstdlib"
+#include "windows.h"
 
 using namespace std;
 
@@ -28,4 +29,23 @@ void Utilities::copyArray(int *arr,  int arrayCopy[], int len) {
     for (int i = 0; i < len; ++i) {
         arrayCopy[i] = arr[i];
     }
+}
+
+void Utilities::startCounter() {
+    PCFreq = 0.0;
+    start = 0;
+
+    LARGE_INTEGER li;
+    if(!QueryPerformanceFrequency(&li))
+        cout << "QueryPerformanceFrequency failed!\n";
+
+    PCFreq = double(li.QuadPart)/1000.0;
+
+    QueryPerformanceCounter(&li);
+    start = li.QuadPart;
+}
+double Utilities::getCounter() {
+    LARGE_INTEGER li;
+    QueryPerformanceCounter(&li);
+    return double(li.QuadPart-start)/PCFreq;
 }
